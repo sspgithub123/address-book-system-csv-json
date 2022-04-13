528ca3fc7bad4334b7f8cb77f3cda5e58b9236d2
Shubham <ssp671995@gmail.com>
2022-04-14 00:34:12 +0530

    Added the open csv read parse to bean in the program.

---
+++ src/com/bridgelabz/OpenCSVReadAndParseToBean.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class OpenCSVReadAndParseToBean {
+    private static final String SAMPLE_CSV_FILE_PATH = "F:\\address-book-system-csv-json\\users-with-header.csv";
+
+    public static void main(String[] args) throws IOException {
+
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH))
+        ) {
+            CsvToBean<CSVUser> csvToBean =  new CsvToBeanBuilder<CSVUser>(reader)
+                    .withType(CSVUser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withSeparator('|')
+                    .build();
+
+            List<CSVUser> csvUsers = csvToBean.parse();
+
+            for(CSVUser csvUser: csvUsers) {
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("PhoneNo : " + csvUser.getPhoneNo());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("==========================");
+            }
+        }
+    }
+}
